95fe3fa0e626ef761a71018854298fcd3b785a55
Quốc Hiệp <44384638+Kydzz@users.noreply.github.com>
2019-03-31 16:04:45 +0700

    Kydz

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict"
+var menu = document.querySelector(".menu");
+var link = document.querySelector(".link");
+console.log(menu);
+menu.addEventListener("click",()=>{
+    link.classList.toggle("hidden");
+})
