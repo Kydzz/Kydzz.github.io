25964c3ed1feafd25e5c89668451f48e650f4941
Quốc Hiệp <44384638+Kydzz@users.noreply.github.com>
2019-04-24 18:55:14 +0700

    kidz

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ var link = document.querySelector(".link");
 var triggle = document.querySelector(".triggle");
 var modal = document.querySelector(".modal");
 var modalclose = document.querySelector(".modal-close");
+
 menu.addEventListener("click",()=>{
     link.classList.toggle("hidden");
 })
 
-navigator.geolocation.getCurrentPosition(function(event){
-})
+// MAP
 function geoMyLocation(){
+    var mapLink = document.querySelector(".map-link");
+    var status = document.querySelector(".status");
     modal.classList.add("show-modal");
     modalclose.addEventListener("click",()=>{
         modal.classList.remove("show-modal");
@@ -19,13 +21,28 @@ function geoMyLocation(){
        if (e.target === modal) 
             modal.classList.remove("show-modal");
    },false)
-
-    function success(){
-
-    }
-    function error(){
-    
-    }
+   mapLink.href = '';
+   mapLink.textContent = '';
+ 
+   function success(position) {
+     const latitude  = position.coords.latitude;
+     const longitude = position.coords.longitude;
+ 
+     status.textContent = '';
+     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+   }
+ 
+   function error() {
+     status.textContent = 'Unable to retrieve your location';
+   }
+ 
+   if (!navigator.geolocation) {
+     status.textContent = 'Geolocation is not supported by your browser';
+   } else {
+     status.textContent = 'Locating…';
+     navigator.geolocation.getCurrentPosition(success, error);
+   }
 }
 
 
