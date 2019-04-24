bea7a3ca726b5d11f66958c96e21fa16fc175f7d
Quốc Hiệp <44384638+Kydzz@users.noreply.github.com>
2019-04-24 17:52:42 +0700

    kidz

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 "use strict"
 var menu = document.querySelector(".menu");
 var link = document.querySelector(".link");
-console.log(menu);
+var triggle = document.querySelector(".triggle");
+var modal = document.querySelector(".modal");
+var modalclose = document.querySelector(".modal-close");
 menu.addEventListener("click",()=>{
     link.classList.toggle("hidden");
 })
 
 navigator.geolocation.getCurrentPosition(function(event){
-
-    
 })
+function geoMyLocation(){
+    modal.classList.add("show-modal");
+    modalclose.addEventListener("click",()=>{
+        modal.classList.remove("show-modal");
+    },false);
+   window.addEventListener("click",(e)=>{
+       if (e.target === modal) 
+            modal.classList.remove("show-modal");
+   },false)
+
+    function success(){
+
+    }
+    function error(){
+    
+    }
+}
+
+
+triggle.addEventListener("click",geoMyLocation);
+
+
