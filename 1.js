5d65e91ca4448cc272b75fe38d950c75411c1b17
Quốc Hiệp <44384638+Kydzz@users.noreply.github.com>
2019-09-15 09:44:47 +0700

    kid1z
    fix

--- 1.js
+++ 1.js
@@ -1,39 +1,7 @@
-new Vue({
-    el:"#app",
-    data: {
-        showNav: true,
-        lastScrollPosition: 0,
-        currentPosition: 0,
-        showBacktotop: true
-    },
-    mounted() {
-        this.lastScrollPosition = window.pageYOffset
-        window.addEventListener("scroll", this.onScroll)
-        
-    },
-    beforeDestroy() {
-        window.removeEventListener("scroll", this.onScroll)
-        
-    },
-    methods: {
-        onScroll(e) {
-            this.currentPosition = window.pageYOffset /* e.target.documentElement.scrollTop */
-            if(window.pageYOffset < 0) return
-            this.showNav = window.pageYOffset < this.lastScrollPosition
-            this.lastScrollPosition  = window.pageYOffset
-            this.showBacktotop = window.pageYOffset < 10
-            // if( window.pageYOffset < this.lastScrollPosition){
-            //     this.showNav = false
-            // }
-            // this.lastScrollPosition = window.pageYOffset
-        },
-        scrollOnTop(){
-                var clearSet = setInterval(function(){
-                    if (window.scrollY > 0)
-                        window.scrollTo(0,window.pageYOffset - 30);
-                    else clearInterval(clearSet)
-                },5)
-        }
-    }
-    
-})
+new Vue({el:"#app",data:{showNav:!0,lastScrollPosition:0,currentPosition:0,showBacktotop:!0},mounted(){this.lastScrollPosition=window.pageYOffset
+window.addEventListener("scroll",this.onScroll)},beforeDestroy(){window.removeEventListener("scroll",this.onScroll)},methods:{onScroll(e){this.currentPosition=window.pageYOffset
+if(window.pageYOffset<0)return
+this.showNav=window.pageYOffset<this.lastScrollPosition
+this.lastScrollPosition=window.pageYOffset
+this.showBacktotop=window.pageYOffset<10},scrollOnTop(){var clearSet=setInterval(function(){if(window.scrollY>0)
+window.scrollTo(0,window.pageYOffset-30);else clearInterval(clearSet)},5)}}})
