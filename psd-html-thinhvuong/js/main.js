22a20ec2c7a772d79d1451977e330e6eda76bfb4
Quốc Hiệp <>
2020-11-03 10:46:26 +0700

    update lan thu n

--- psd-html-thinhvuong/js/main.js
+++ psd-html-thinhvuong/js/main.js
@@ -143,35 +143,3 @@ $(window).scroll(function() {
   }
 });
 
-// animation scroll number counter
-var counted = 0;
-$(window).scroll(function() {
-
-  var oTop = $('.achievement__link').offset().top - window.innerHeight;
-  if ($(window).scrollTop() > oTop) {
-    $('.achievement__list').removeClass('animate');
-  }
-  if (counted == 0 && $(window).scrollTop() > oTop) {
-    $('.count').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-        {
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-          }
-
-        });
-    });
-    counted = 1;
-  }
-});
---
+++ psd-html-thinhvuong/js/scrollcounter.js
@@ -0,0 +1,32 @@
+// animation scroll number counter
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.achievement__link').offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    $('.achievement__list').removeClass('animate');
+  }
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+
+        });
+    });
+    counted = 1;
+  }
+});
