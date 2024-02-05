07f73d0549f69eb5f1ed9fa6f5492a113326af25
Quốc Hiệp <44384638+kid1z@users.noreply.github.com>
2024-02-06 03:36:37 +0700

    update

--- nongdocon/index.js
+++ nongdocon/index.js
@@ -1,5 +1,36 @@
+// calculation
+const STANDARD_DRINK_G = 10;
+const METABOLIC_REMOVAL_RATE_GPH = 7.9;		// in g/hr
+const WATER_CONTENT_OF_BLOOD = 0.8157;
+const ALCOHOL_SPECIFIC_GRAVITY = 0.79; // gam/ml
+
+// info
+const gender = $('gender');
+const age = $('age');
+const weight = $('weight');
+const height = $('height');
+const beerSelect = $('beerSelect');
+const quatitySelect = $('quatitySelect');
+const alcoholSelect = $('alcoholSelect');
+const glassSelect = $('glassSelect');
+const alcolholQuatitySelect = $('alcolholQuatitySelect');
+const minutes = $('minutes');
+// submit
+const submit = document.getElementById('submit');
+
+
+// beer
+const drink = document.getElementById('drink');
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   createHeight();
+  createWeight();
+})
+
+submit.addEventListener('click', () => {
+  handleSubmit();
 })
 
 function createWeight() {
@@ -35,3 +66,56 @@ function createHeight() {
     i++;
   };
 };
+
+function handleChangeType() {
+  // alcolhol
+  const alcoholTypes = $('alcohol');
+  const glass = $('glass');
+  const ancolholQuatity = $('ancolholQuatity');
+
+  // beer
+  const beer = $('beer');
+  const quatity = $('quatity');
+
+  if (drink.value === 'beer') {
+    beer.classList.remove('hidden');
+    quatity.classList.remove('hidden');
+
+    alcoholTypes.classList.add('hidden');
+    glass.classList.add('hidden');
+    ancolholQuatity.classList.add('hidden');
+  } else {
+    beer.classList.add('hidden');
+    quatity.classList.add('hidden');
+
+    alcoholTypes.classList.remove('hidden');
+    glass.classList.remove('hidden');
+    ancolholQuatity.classList.remove('hidden');
+  }
+}
+
+function handleSubmit() {
+  const params = {
+    gender: gender.value,
+    age: age.value,
+    weight: weight.value,
+    height: height.value,
+    beerSelect: beerSelect.value,
+    quatitySelect: quatitySelect.value,
+    alcoholSelect: alcoholSelect.value,
+    glassSelect: glassSelect.value,
+    alcolholQuatitySelect: alcolholQuatitySelect.value,
+    minutes: minutes.value,
+  }
+
+  console.log("params: ", params)
+  Swal.fire({
+    title: "Hi!",
+    text: "Tính năng đang hoàn thiện, chờ chút nhé!",
+    icon: "success"
+  });
+}
+
+function $(element) {
+  return document.getElementById(element);
+}
